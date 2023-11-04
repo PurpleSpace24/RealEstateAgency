e77ad45c3862653a0af5a142d84388fc29959306
Yana Trayanova <73670359+PurpleSpace24@users.noreply.github.com>
2023-11-04 18:25:53 +0200

    Update Broker.java

--- src/main/java/Entity/Broker.java
+++ src/main/java/Entity/Broker.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Entity;
 
 import java.util.Comparator;
@@ -50,52 +49,3 @@ public class Broker {
         }
     };
 }
-=======
-package Entity;
-
-import java.util.Comparator;
-import java.util.Random;
-
-public class Broker {
-    private long regNumber;
-    private static int nextId = 101;
-
-    private String name;
-
-    public Broker() {
-        Random random = new Random();
-        nextId = random.nextInt(101);
-        this.regNumber = nextId;
-    }
-
-    public long getRegNumber() {
-        return regNumber;
-    }
-
-    public void setRegNumber(long regNumber) {
-        this.regNumber = regNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Broker{" +
-                "id=" + regNumber +
-                ", name='" + name + '\'' +
-                '}';
-    }
-    public static Comparator<Broker> brokerRegNumber = new Comparator<Broker>() {
-        @Override
-        public int compare(Broker broker, Broker broker1) {
-            return Long.compare(broker.getRegNumber(),(broker1.getRegNumber()));
-        }
-    };
-}
->>>>>>> origin/main
