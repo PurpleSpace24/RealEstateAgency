0d697033d9adf90188ad5a3da9f4694f9cf6aa47
Yana Trayanova <73670359+PurpleSpace24@users.noreply.github.com>
2023-11-10 16:32:34 +0200

    Add files via upload

---
+++ src/main/java/Entity/Apartment.java
@@ -0,0 +1,14 @@
+package Entity;
+
+public class Apartment extends RealEstate {
+
+    public Apartment() {
+    }
+
+    public Apartment(String address, long regNumber, double price_per_square_meter, double area) {
+        super(address, regNumber, price_per_square_meter, area);
+    }
+
+
+
+}
